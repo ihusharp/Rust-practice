c301ea478a17510d007fd21a8188a3225ecd8fdc
husharp <jinhao.hu@pingcap.com>
2022-09-16 08:13:21 +0800

    [percolator] finished server function get
    Signed-off-by: husharp <jinhao.hu@pingcap.com>

--- courses/dss/percolator/src/server.rs
+++ courses/dss/percolator/src/server.rs
@@ -1,4 +1,5 @@
 use std::collections::BTreeMap;
+use std::ops::{RangeBounds, Bound};
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
@@ -24,7 +25,7 @@ impl timestamp::Service for TimestampOracle {
     async fn get_timestamp(&self, _: TimestampRequest) -> labrpc::Result<TimestampResponse> {
         // Your code here.
         Ok(TimestampResponse {
-            timestamp: self.timestamp_generator.fetch_add(1, Ordering::Relaxed),
+            timestamp: self.timestamp_generator.fetch_add(1, Ordering::SeqCst),
         })
     }
 }
@@ -36,6 +37,22 @@ pub type Key = (Vec<u8>, u64);
 pub enum Value {
     Timestamp(u64),
     Vector(Vec<u8>),
+}
+
+impl Value {
+    fn as_ts(&self) -> u64 {
+        match self {
+            Value::Timestamp(ts) => *ts,
+            _ => panic!("not a timestamp"),
+        }
+    }
+
+    fn as_vec(&self) -> Vec<u8> {
+        match self {
+            Value::Vector(v) => v.clone(),
+            _ => panic!("not a vector"),
+        }
+    }
 }
 
 pub enum Column {
@@ -54,6 +71,16 @@ pub struct KvTable {
 }
 
 impl KvTable {
+    fn column_ref(&self, column: Column) -> &BTreeMap<Key, Value> {
+        match column {
+            Column::Write => &self.write,
+            Column::Data => &self.data,
+            Column::Lock => &self.lock,
+        }
+    }
+}
+
+impl KvTable {
     // Reads the latest key-value record from a specified column
     // in MemoryStorage with a given key and a timestamp range.
     #[inline]
@@ -61,12 +88,25 @@ impl KvTable {
         &self,
         key: Vec<u8>,
         column: Column,
-        ts_start_inclusive: Option<u64>,
-        ts_end_inclusive: Option<u64>,
+        ts_range: impl RangeBounds<u64>
     ) -> Option<(&Key, &Value)> {
         // Your code here.
-        unimplemented!()
+        let column = self.column_ref(column);
+        let key_start = match ts_range.start_bound() {
+            Bound::Included(ts) => Bound::Included((key.to_vec(), *ts)),
+            Bound::Excluded(ts) => Bound::Excluded((key.to_vec(), *ts)),
+            Bound::Unbounded => Bound::Included((key.to_vec(), 0)),
+        };
+
+        let key_end = match ts_range.end_bound() {
+            Bound::Included(ts) => Bound::Included((key.to_vec(), *ts)),
+            Bound::Excluded(ts) => Bound::Excluded((key.to_vec(), *ts)),
+            Bound::Unbounded => Bound::Included((key.to_vec(), u64::MAX)),
+        };
+
+        column.range((key_start, key_end)).last()
     }
+        
 
     // Writes a record to a specified column in MemoryStorage.
     #[inline]
@@ -95,7 +135,28 @@ impl transaction::Service for MemoryStorage {
     // example get RPC handler.
     async fn get(&self, req: GetRequest) -> labrpc::Result<GetResponse> {
         // Your code here.
-        unimplemented!()
+        let GetRequest { key, start_ts } = req;
+
+        let data = loop {
+            let data = self.data.lock().unwrap();
+            let lock = data.read(key.clone(), Column::Lock, 0..=start_ts);
+            if lock.is_none() {
+                break data;
+            } else {
+                // Back off.
+                self.back_off_maybe_clean_up_lock(start_ts, key.clone());
+            }
+        };
+
+        let value = if let Some((_, write)) = data.read(key.clone(), Column::Write, 0..=start_ts) {
+            let data_ts = write.as_ts();
+            let value = data.read(key.clone(), Column::Data, data_ts..=data_ts).unwrap().1;
+            value.as_vec().to_vec()
+        } else {
+            vec![]
+        };
+
+        Ok(GetResponse { value })
     }
 
     // example prewrite RPC handler.
