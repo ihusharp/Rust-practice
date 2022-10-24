58ff6cc8b6740f2e46e880f0f29ddf5b5186f9c4
husharp <jinhao.hu@pingcap.com>
2022-10-24 08:16:53 +0800

    [rust_os]finished vga-text-mode
    Signed-off-by: husharp <jinhao.hu@pingcap.com>

--- os_rust/src/main.rs
+++ os_rust/src/main.rs
@@ -1,26 +1,19 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+    println!("Hello World");
+    panic!("Some panic message");
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> !{
+fn panic(info: &PanicInfo) -> !{
+    println!("woops!!! {}", info);
     loop {}
 }
-
