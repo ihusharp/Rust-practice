5cfbe8bab9c6caba86efa8746894e4b7bbb0a2b6
husharp <jinhao.hu@pingcap.com>
2022-10-27 10:53:52 +0800

    [rust_os]finished cpu-exceptions
    Signed-off-by: husharp <jinhao.hu@pingcap.com>

--- os_rust/src/lib.rs
+++ os_rust/src/lib.rs
@@ -1,15 +1,22 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
+#![feature(abi_x86_interrupt)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
+
+pub fn init() {
+    interrupts::init_idt();
+}
 
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
 
     loop {}
