5a47414e3e9b6055af83200fe7dd90ab3527461b
husharp <ihusharp@gmail.com>
2023-06-29 10:02:05 +0800

    [async future] impl TimerFuture
    Signed-off-by: husharp <ihusharp@gmail.com>

---
+++ async_future/src/lib.rs
@@ -0,0 +1,53 @@
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::{Arc, Mutex},
+    task::{Context, Poll, Waker},
+    thread,
+    time::Duration,
+};
+
+pub struct TimerFuture {
+    shared_state: Arc<Mutex<SharedState>>,
+}
+
+struct SharedState {
+    completed: bool,
+    waker: Option<Waker>,
+}
+
+impl Future for TimerFuture {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut shared_state = self.shared_state.lock().unwrap();
+        if shared_state.completed {
+            Poll::Ready(())
+        } else {
+            println!("TimerFuture::poll: {:?}", thread::current().id());
+            shared_state.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
+
+impl TimerFuture {
+    pub fn new(duration: Duration) -> Self {
+        let shared_state = Arc::new(Mutex::new(SharedState {
+            completed: false,
+            waker: None,
+        }));
+        let thread_shared_state = shared_state.clone();
+        thread::spawn(move || {
+            println!("TimerFuture::new: {:?}", thread::current().id());
+            thread::sleep(duration);
+            let mut shared_state = thread_shared_state.lock().unwrap();
+            shared_state.completed = true;
+            if let Some(waker) = shared_state.waker.take() {
+                println!("TimerFuture::new: wake: {:?}", thread::current().id());
+                waker.wake();
+            }
+        });
+
+        TimerFuture { shared_state }
+    }
+}
---
+++ async_future/src/main.rs
@@ -0,0 +1,87 @@
+use std::thread;
+
+use async_future::TimerFuture;
+
+use {
+    futures::{
+        future::{BoxFuture, FutureExt},
+        task::{waker_ref, ArcWake},
+    },
+    std::{
+        future::Future,
+        sync::mpsc::{sync_channel, Receiver, SyncSender},
+        sync::{Arc, Mutex},
+        task::Context,
+        time::Duration,
+    },
+};
+
+struct Spawner {
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+impl Spawner {
+    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+        let task = Arc::new(Task {
+            future: Mutex::new(Some(future.boxed())),
+            task_sender: self.task_sender.clone(),
+        });
+        self.task_sender.send(task).expect("queue is full");
+    }
+}
+
+struct Executer {
+    ready_queue: Receiver<Arc<Task>>,
+}
+
+impl Executer {
+    fn run(&self) {
+        while let Ok(task) = self.ready_queue.recv() {
+            println!("run: {:?}", thread::current().id());
+            let mut future = task.future.lock().unwrap();
+            // Check future is some
+            if let Some(mut f) = future.take() {
+                let waker = waker_ref(&task);
+                let context = &mut Context::from_waker(&*waker);
+                println!("run: poll: {:?}", thread::current().id());
+                if f.as_mut().poll(context).is_pending() {
+                    println!("run: pending: {:?}", thread::current().id());
+                    // Pending for next poll
+                    *future = Some(f);
+                }
+            }
+        }
+    }
+}
+
+struct Task {
+    future: Mutex<Option<BoxFuture<'static, ()>>>,
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        println!("wake_by_ref: {:?}", thread::current().id());
+        arc_self
+            .task_sender
+            .send(arc_self.clone())
+            .expect("queue is full");
+    }
+}
+
+fn new_executer_and_spawner() -> (Executer, Spawner) {
+    let (task_sender, ready_queue) = sync_channel(100);
+    (Executer { ready_queue }, Spawner { task_sender })
+}
+
+fn main() {
+    let (executer, spawner) = new_executer_and_spawner();
+    spawner.spawn(async {
+        println!("start!");
+        TimerFuture::new(Duration::from_secs(2)).await;
+        println!("end!");
+    });
+
+    drop(spawner);
+    executer.run();
+}
